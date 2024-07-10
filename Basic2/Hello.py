58f312a8121f81b0e04b5adf7d3e5c777ddbde9f
Jesus Estrada <estrada.jesus1312@gmail.com>
2024-07-09 19:09:56 -0600

    Day 1: Introduction

---
+++ Basic2/Hello.py
@@ -0,0 +1,32 @@
+# Esto es un comentario 
+print("Hola mundo")
+"""
+Este es un comentario 
+en varias lineas
+"""
+
+nombre = "Jesus"
+numero = 5
+
+if(nombre == "Jesus" and numero > 0):
+    print("El es Jesus")
+
+# Consultar el tipo de dato
+print(type("Hola")) # Tipo de dato 'str'
+print(type(5)) # Tipo de dato 'int'
+print(type(5.9)) # Tipo de dato 'float'
+print(type(1 + 3j)) # Tipo de dato 'complex'
+print(type(['1', '2', '3'])) # Tipo de dato 'list'
+print(type([1,2,3])) # Tipo de dato 'list'
+print(type({'name': 'Don chuy'})) # Tipo de dato 'dict'
+print(type({9.8, 9.9, 3.1, 2.4})) # Tipo de dato 'set'
+print(type((9.4, 3.4, 4.3))) # Tipo de datos 'tuple'
+
+# Operations
+print(5 + 10) # addition
+print(10 - 5) # subtraction
+print(2 * 3) # multiplication
+print(3 / 2) # division
+print(3 ** 2) # exponential
+print(3 % 2) # modulus
+print(3 // 2) # floor division operator
